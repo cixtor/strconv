22e1bdb4cf7d5d213ecbc1f512f27c6f6500afc7
Cixtor <cixtords@gmail.com>
2020-03-11 09:00:00 -0800

    Fix unit-tests after addition of command line arguments tracker

--- main_test.go
+++ main_test.go
@@ -7,7 +7,9 @@ const text = "tf GO1Qnn7S60 WqsxKdVFQ7 nnnFQnsVD1n5MPpVqBnNrraQd2"
 func TestReplace(t *testing.T) {
 	var app Application
 
-	if app.Replace(text, "n", "@") != "tf GO1Q@@7S60 WqsxKdVFQ7 @@@FQ@sVD1@5MPpVqB@NrraQd2" {
+	app.args = []string{"n", "@"}
+
+	if app.Replace(text) != "tf GO1Q@@7S60 WqsxKdVFQ7 @@@FQ@sVD1@5MPpVqB@NrraQd2" {
 		t.Fatalf("Replace did not run as expected")
 	}
 }
