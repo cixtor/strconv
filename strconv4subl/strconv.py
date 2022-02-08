eef8513b0eef8e3aa116b88ad107dfcea359b8e6
Cixtor <cixtords@gmail.com>
2022-02-08 10:34:28 -0800

    Add Sublime Text plugin to execute all the commands provided by strconv

---
+++ strconv4subl/strconv.py
@@ -0,0 +1,40 @@
+import sublime
+import sublime_plugin
+import subprocess
+
+
+class StrconvCommand(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+		selections = self.view.sel()
+		if len(selections) == 0:
+			return
+		for region in selections:
+			the_text = self.view.substr(region)
+			if the_text == "":
+				self.set_status("nothing to format")
+				continue
+			the_text = self.external_command(the_text, **args)
+			region_text = self.view.substr(region)
+			self.view.replace(edit, region, the_text)
+			self.set_status("done")
+
+
+	def external_command(self, the_text, **args):
+		p = subprocess.Popen(
+			["strconv", args.get("action")],
+			stdin=subprocess.PIPE,
+			stderr=subprocess.PIPE,
+			stdout=subprocess.PIPE,
+		)
+		out, err = p.communicate(input=the_text)
+		if p.returncode != 0:
+			self.set_status(err.split("\n")[0])
+			return the_text
+		return out
+
+
+	def set_status(self, message):
+		self.view.set_status("strconv_output", message)
+		sublime.set_timeout_async(
+			lambda: self.view.erase_status("strconv_output"), 5000
+		)
